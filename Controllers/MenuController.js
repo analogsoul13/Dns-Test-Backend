b914ea4749a57f6f6020a7ea50dcc7d58e487407
analogsoul13 <analogsoul13@gmail.com>
2025-01-27 12:48:50 +0530

    Add items to menu

--- Controllers/MenuController.js
+++ Controllers/MenuController.js
@@ -4,7 +4,7 @@ const Menu = require('../Models/Menu')
 exports.createMenu = async (req, res) => {
     try {
         const { name, description } = req.body
-        const menu = new Menu({name, description})
+        const menu = new Menu({ name, description })
         await menu.save()
         res.status(201).json(menu)
     } catch (error) {
@@ -12,7 +12,7 @@ exports.createMenu = async (req, res) => {
     }
 }
 
-exports.getAllMenu = async (req,res) => {
+exports.getAllMenu = async (req, res) => {
     try {
         const menus = await Menu.find()
         res.json(menus)
@@ -21,11 +21,22 @@ exports.getAllMenu = async (req,res) => {
     }
 }
 
-exports.getMenuItems = async (req,res) => {
+exports.getMenuItems = async (req, res) => {
     try {
-        const items = await Item.find({menuId: req.params.id})
+        const items = await Item.find({ menuId: req.params.id })
         res.json(items)
     } catch (error) {
         res.status(500).json("Error loading menu items", error)
+    }
+}
+
+exports.addMenuItem = async (req, res) => {
+    try {
+        const { name, description, price } = req.body
+        const item = new Item({ menuId: req.params.id, name, description, price })
+        await item.save()
+        res.status(201).json(item)
+    } catch (error) {
+        res.status("Error adding item", error)
     }
 }
